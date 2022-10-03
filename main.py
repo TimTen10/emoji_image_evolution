aaff170f73b5d6efc1d87226ebcd6ca7d1c242aa
Tim Eckle <Tim.Eckle94@gmail.com>
2022-10-03 11:46:54 +0200

    Started to add necessary functions.

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 """
 Basic idea:
-Recreate an image by approximating it using scaled and rotates emojis as building blocks.
+Recreate an image by approximating it using scaled and rotates building_blocks as building blocks.
 The "emoji image" will be built one emoji after another using a genetic algorithm like approach.
-All emojis found in the "emojis" folder can be used as parents / mutations.
+All building_blocks found in the "building_blocks" folder can be used as parents / mutations.
 
 Steps:
-1.) Load original image and turn it into height x width x colors matrix (colors = 3 if mode RGB, = 4 if mode RGBA)
-2.1) Create new (empty) image with the same width and height
-2.2) Put one of the emojis (transformed) onto it
+1. Load original image and turn it into height x width x colors matrix (colors = 3 if mode RGB, = 4 if mode RGBA)
+2.1 Create new (empty) image with the same width and height
+2.2 Put one of the building_blocks (transformed) onto it
     - possible transformations:
         - rotate
         - size
         - position
         -> randomly create transformations
-3.) Compare the new image and the original
+3. Compare the new image and the original
     - Repeat for every version of the new image
-4.) Choose best one
+4. Choose best one
     - Need function to determine the "best" one (pixel value diff.?)
-5.) Repeat until new image is close enough to original image
+5. Repeat until new image is close enough to original image
 """
 
 import numpy as np
 from PIL import Image
+
+
+def load_original(filepath: str) -> np.ndarray:
+    original_image = Image.open(filepath)
+    original_pixel = np.array(original_image)  # Creates numpy array of pixels
+    return original_pixel
+
+
+def initialize_population(*, image_size: tuple[int, int], population_size: int) -> list[np.ndarray]:
+    return [np.zeros(image_size) for _ in range(population_size)]
+
+
+def transform(to_add: np.ndarray) -> np.ndarray:
+    # TODO: Might want to have PIL.Image as input instead of np.ndarray for rotation and scaling purpose
+    # Position can not really be transformed in here
+    pass
+
+
+def score(original_image_pixels: np.ndarray, approach_image_pixels: np.ndarray) -> float:
+    pass
 
 
 def main():
