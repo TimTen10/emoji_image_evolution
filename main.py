f669b7cde5bfc477217aec7638873490fd1b595b
Tim Eckle <Tim.Eckle94@gmail.com>
2022-10-03 20:34:44 +0200

    Rough base version. Running but slow.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """
 Basic idea:
-Recreate an image by approximating it using scaled and rotates building_blocks as building blocks.
+Recreate an image by approximating it using scaled and rotates components as building blocks.
 The "emoji image" will be built one emoji after another using a genetic algorithm like approach.
-All building_blocks found in the "building_blocks" folder can be used as parents / mutations.
+All components found in the "components" folder can be used as parents / mutations.
 
 Steps:
-1. Load original image and turn it into height x width x colors matrix (colors = 3 if mode RGB, = 4 if mode RGBA)
+1. Load original image and turn it into height x width x colors matrix (colors = 4 if mode RGBA)
 2.1 Create new (empty) image with the same width and height
-2.2 Put one of the building_blocks (transformed) onto it
+2.2 Put one of the components (transformed) onto it
     - possible transformations:
         - rotate
         - size
@@ -23,13 +23,25 @@ Steps:
 import numpy as np
 from PIL import Image
 
+import glob
+import random
 from typing import Callable
 
 
 def load_original(filepath: str) -> np.ndarray:
-    original_image = Image.open(filepath)
-    original_pixel = np.array(original_image)  # Creates numpy array of pixels
+    original_image = Image.open(filepath).convert('RGBA')
+    original_pixel = np.array(original_image, dtype=np.ubyte)  # Creates numpy array of pixels
     return original_pixel
+
+
+def load_components(folderpath: str) -> list[Image]:
+    """
+    This method loads all '.png' as jpeg Images into a list.
+
+    :param folderpath:
+    :return: List of PIL.Images in RGB mode
+    """
+    return [Image.open(component) for component in glob.iglob(f"{folderpath}/*.png")]
 
 
 def initialize_population(*, image_size: tuple[int, int], population_size: int) -> list[np.ndarray]:
@@ -40,12 +52,31 @@ def initialize_population(*, image_size: tuple[int, int], population_size: int) 
     :param population_size: Anzahl der Elemente in der Population
     :return: Liste aus 0er Arrays in der Größe des original Bildes (entspricht komplett schwarzen Bildern)
     """
-    return [np.zeros(image_size) for _ in range(population_size)]
+    return [np.zeros(image_size, dtype=np.ubyte) for _ in range(population_size)]
 
 
-def transform(to_add: np.ndarray) -> np.ndarray:
-    # TODO: Might want to have PIL.Image as input instead of np.ndarray for rotation and scaling purpose
-    pass
+def transform(base_image: np.ndarray, component: Image) -> np.ndarray:
+    # Rotate:
+    component = component.rotate(angle=random.randrange(360))
+
+    # Scale:
+    scaling_factor = random.randint(10, 1000) / 100
+    new_dimensions = (int(component.width * scaling_factor), int(component.height * scaling_factor))
+    component = component.resize(new_dimensions)
+
+    # Position:
+    height, width, _ = base_image.shape
+    # Create
+    canvas = np.zeros((height + component.height, width + component.width, 4), dtype=np.ubyte)
+    x_pos = random.randint(0, width)
+    y_pos = random.randint(0, height)
+    canvas[y_pos:y_pos+component.height, x_pos:x_pos+component.width] = component
+    # Cut into shape
+    canvas = canvas[component.height // 2:-component.height // 2, component.width // 2:-component.width // 2]
+    canvas_image = Image.fromarray(canvas)
+    transformed_image = Image.fromarray(base_image)
+    transformed_image.paste(canvas_image, (0, 0), canvas_image)
+    return np.array(transformed_image, dtype=np.ubyte)
 
 
 def score(original_image_pixels: np.ndarray, approach_image_pixels: np.ndarray) -> int:
@@ -60,8 +91,43 @@ def score(original_image_pixels: np.ndarray, approach_image_pixels: np.ndarray) 
 
 
 def evolve(population: list[np.ndarray],
-           scoring_function: Callable[[np.ndarray, np.ndarray], float]) -> np.ndarray:
-    pass
+           original: np.ndarray,
+           components: list[Image],
+           scoring_function: Callable[[np.ndarray, np.ndarray], float]) -> [np.ndarray, list[np.ndarray]]:
+    scored = []
+    for individual in population:
+        scored.append((individual, scoring_function(original, individual)))
+
+    scored.sort(key=lambda x: x[1], reverse=True)  # Sort scored individuals, best scores at the front
+    parent = scored[0][0]
+
+    next_gen = []
+    for comp in components:
+        for _ in range(len(population) // len(components)):
+            next_gen.append(transform(parent, comp))
+
+    return parent, next_gen
+
+
+def run():
+    # TODO: Filenames via sysargs
+    orig_name = "pokemon_ending_screen"
+    orig_pix = load_original(f"original/{orig_name}.jpg")
+
+    components = load_components("components")
+
+    # Evolution
+    epochs = 1000
+    population_size = 100
+    population = initialize_population(image_size=orig_pix.shape,
+                                       population_size=population_size)
+
+    for e in range(epochs + 1):
+        best_image, population = evolve(population, orig_pix, components, score)
+        if e % 2 == 0:
+            best_image = Image.fromarray(best_image)
+            best_image.save(f"output_images/{orig_name}_{e}.png")
+        print(f"Done with Epoch {e}")
 
 
 def main():
@@ -76,4 +142,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    run()
